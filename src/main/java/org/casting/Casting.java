13ddde7a838002e672565b812d724394d65be88a
skillfuldev <chernonog.evgeniy@gmail.com>
2023-11-18 18:09:54 +0200

    Add_part1

---
+++ src/main/java/org/casting/Casting.java
@@ -0,0 +1,17 @@
+package org.casting;
+
+public class Casting {
+    public static void main(String[] args) {
+       int x = 323;
+       short y = (short)x;
+
+       byte i = (byte)x;
+        System.out.println(i);
+        char d = '1';
+
+
+        System.out.println(d);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
